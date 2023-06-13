5b25cbbf71e5d96504b7b57d73ee61eeecb8b2f3
Trần Văn Lộc <tranvanloc96lhp@gmail.com>
2023-06-13 14:37:04 +0700

    up

--- resources/views/admin/NewContact.blade.php
+++ resources/views/admin/NewContact.blade.php
@@ -18,17 +18,58 @@
 
     <!-- Main content -->
     <div class="content">
+        <div class="row">
+            <div class="col-12">
+                <div class="card">
+                    <div class="card-header">
+                        <h3 class="card-title">Tổng cộng : 9999 đơn</h3>
 
+                        <div class="card-tools">
+                            <div class="input-group input-group-sm" style="width: 150px;">
+                                <input type="text" name="table_search" class="form-control float-right"
+                                    placeholder="Tìm kiếm">
 
-
-
-
-
-
-
-
-
-
+                                <div class="input-group-append">
+                                    <button type="submit" class="btn btn-default">
+                                        <i class="bi bi-search"></i>
+                                    </button>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <!-- /.card-header -->
+                    <div class="card-body table-responsive p-0" style="height: 70vh;">
+                        <table class="table table-head-fixed text-nowrap">
+                            <thead>
+                                <tr>
+                                    <th>ID Đơn</th>
+                                    <th>Họ và tên</th>
+                                    <th>Email</th>
+                                    <th>Số điện thoại</th>
+                                    <th>Góp ý</th>
+                                    <th>Thời gian nhận</th>
+                                    <th></th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                <tr>
+                                    <td>183</td>
+                                    <td>John Doe</td>
+                                    <td>11-7-2014</td>
+                                    <td><span class="tag tag-success">Approved</span></td>
+                                    <td>Bacon ipsum dolor sit amet salami venison chicken flank fatback doner.</td>
+                                    <td>a</td>
+                                    <td>a</td>
+                                </tr>
+                                
+                            </tbody>
+                        </table>
+                    </div>
+                    <!-- /.card-body -->
+                </div>
+                <!-- /.card -->
+            </div>
+        </div>
         <!-- /.container-fluid -->
     </div>
     <!-- /.content -->
