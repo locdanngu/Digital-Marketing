2a6081d02b8251a748ae80b2d58accfa2441badd
Trần Văn Lộc <tranvanloc96lhp@gmail.com>
2023-06-19 16:35:13 +0700

    up

--- resources/views/admin/BlogAdmin.blade.php
+++ resources/views/admin/BlogAdmin.blade.php
@@ -49,11 +49,40 @@
                                     <th>Nội dung</th>
                                     <th>Thời gian</th>
                                     <th>Người đăng</th>
+                                    <th>Avatar</th>
                                     <th></th>
                                 </tr>
                             </thead>
                             <tbody id="capnhat">
-
+                                @foreach($listblog as $lb)
+                                <td>{{ $lb->idblog }}</td>
+                                <td><img src="{{ $lb->imageblog }}" class="fixanhnen"></td>
+                                <td>{{ $lb->title }}</td>
+                                <td>{!! nl2br(substr($lb->content, 0, 20)) !!}...</td>
+                                <td>{{ $lb->timeread }}</td>
+                                <td>{{ $lb->user->name }}</td>
+                                <td><img src="{{ $lb->user->avatar }}" class="lamtronavatar"></td>
+                                <td>
+                                    <div class="d-flex justify-content-between">
+                                        <button class="btn btn-primary btn-sm" type="button" data-toggle="modal"
+                                            data-target="#modal-change-blog" data-id="{{ $lb->idblog }}"
+                                            data-title="{{ $lb->title }}" data-content="{{$lb->content}}"
+                                            data-timeread="{{ $lb->timeread }}" data-imageblog="{{ $lb->imageblog }}">
+                                            <i class="bi bi-pencil"></i>
+                                            Sửa
+                                        </button><button class="btn btn-info btn-sm" type="button" data-toggle="modal"
+                                            data-target="#modal-info-blog">
+                                            <i class="bi bi-info"></i>
+                                            Xem
+                                        </button>
+                                        <button class="btn btn-danger btn-sm" type="button" data-toggle="modal"
+                                            data-target="#modal-delete-blog">
+                                            <i class="bi bi-trash"></i>
+                                            Xóa
+                                        </button>
+                                    </div>
+                                </td>
+                                @endforeach
                             </tbody>
                         </table>
                     </div>
@@ -73,8 +102,7 @@
 <!-- Modal trả lời đơn tư vấn -->
 <div class="modal fade" id="modal-add-blog">
     <div class="modal-dialog">
-        <form class="modal-content" action="{{ route('blog.add') }}" method="post"
-            enctype="multipart/form-data">
+        <form class="modal-content" action="{{ route('blog.add') }}" method="post" enctype="multipart/form-data">
             @csrf
             <div class="modal-header">
                 <h4 class="modal-title">Tạo bài viết</h4>
@@ -116,6 +144,49 @@
 </div>
 
 
+<div class="modal fade" id="modal-change-blog">
+    <div class="modal-dialog">
+        <form class="modal-content" action="{{ route('blog.add') }}" method="post" enctype="multipart/form-data">
+            @csrf
+            <div class="modal-header">
+                <h4 class="modal-title">Chỉnh sửa bài viết</h4>
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                </button>
+            </div>
+            <div class="modal-body">
+                <div class="input-group mb-3">
+                    <span class="input-group-text" id="inputGroup-sizing-default">Tiêu đề</span>
+                    <input type="text" class="form-control" aria-label="Sizing example input"
+                        aria-describedby="inputGroup-sizing-default" required name="title">
+                </div>
+                <div class="input-group mb-3">
+                    <span class="input-group-text" id="inputGroup-sizing-default">Nội dung</span>
+                    <textarea type="text" class="form-control" aria-label="Sizing example input"
+                        aria-describedby="inputGroup-sizing-default" required name="content"></textarea>
+                </div>
+                <div class="input-group mb-3">
+                    <span class="input-group-text" id="inputGroup-sizing-default">Thời gian đọc</span>
+                    <input type="number" class="form-control" aria-label="Sizing example input"
+                        aria-describedby="inputGroup-sizing-default" required name="timeread">
+                </div>
+                <div class="input-group mb-3">
+                    <span class="input-group-text" id="inputGroup-sizing-default">Ảnh nền</span>
+                    <input class="form-control" type="file" id="formFile" accept="image/*" style="max-width:100%"
+                        onchange="previewImage2(event)" name="image" required>
+                </div>
+                <img id="preview2" src="" alt="" style="max-width:30%;margin-bottom:1em" class="imageblog">
+            </div>
+            <div class="modal-footer justify-align-content-end">
+                <button type="button" class="btn btn-default" data-dismiss="modal">Đóng</button>
+                <button type="submit" class="btn btn-primary">Hoàn tất</button>
+            </div>
+        </form>
+        <!-- /.modal-content -->
+    </div>
+    <!-- /.modal-dialog -->
+</div>
+
 
 
 @endsection
@@ -132,6 +203,54 @@ function previewImage(event) {
     }
     reader.readAsDataURL(file);
 }
+
+function previewImage2(event) {
+    const preview = document.getElementById('preview2');
+    const file = event.target.files[0];
+    const reader = new FileReader();
+    reader.onload = function() {
+        preview.src = reader.result;
+    }
+    reader.readAsDataURL(file);
+}
+
+$(document).ready(function() {
+    $('#modal-change-blog').on('shown.bs.modal', function(event) {
+        var button = $(event.relatedTarget); // Nút "Change" được nhấn
+        var id = button.data('id');
+        var title = button.data('title');
+        var content = button.data('content');
+        var timeread = button.data('timeread');
+        var imageblog = button.data('imageblog');
+        var modal = $(this);
+        modal.find('input[name="id"]').val(id);
+        modal.find('input[name="title"]').val(title);
+        modal.find('textarea[name="content"]').val(content);
+        modal.find('input[name="timeread"]').val(timeread);
+        modal.find('img.imageblog').attr('src', imageblog);
+    });
+
+
+    $('#search').on('input', function() {
+        var search = $(this).val();
+        $.ajax({
+            type: 'POST',
+            url: '{{ route("oldcontact.search") }}',
+            data: {
+                _token: '{{ csrf_token() }}',
+                search: search
+            },
+            success: function(response) {
+                var html = response.html;
+                var count = response.count;
+                $('#capnhat').html(html);
+                $('.card-title').text('Tổng cộng: ' + count + ' đơn');
+            },
+            error: function(xhr, status, error) {}
+        });
+    });
+
+});
 </script>
 
 @endsection
