a874401cd80424b45204c1690643f58f9bada1dd
Trần Văn Lộc <tranvanloc96lhp@gmail.com>
2023-06-19 14:10:06 +0700

    up

--- resources/views/Traloituvan.blade.php
+++ resources/views/Traloituvan.blade.php
@@ -4,7 +4,7 @@
     <p style="color: #333333;">Đây là trả lời cho đơn đăng ký tư vấn bạn đã gửi lúc: {{ $time }}</p>
 
     <blockquote style="background-color: #f5f5f5; border-left: 5px solid #cccccc; padding: 10px;">
-        <p style="color: #333333;">{{ $requestmessage }}.</p>
+        <p style="color: #333333;">{!! nl2br($requestmessage) !!}.</p>
     </blockquote>
 
     <p style="color: #333333;">Cảm ơn bạn đã tin tưởng dịch của chúng tôi.</p>
