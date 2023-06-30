0e859d67aa7a5770e4830a3f3b957f5a895300e6
Trần Văn Lộc <tranvanloc96lhp@gmail.com>
2023-06-30 11:19:50 +0700

    up

--- resources/views/Contentblog.blade.php
+++ resources/views/Contentblog.blade.php
@@ -26,53 +26,9 @@
                 <a href="" class="share"><i class="bi bi-share-fill"></i></a>
             </div>
             <div class="bodybox1">
-                <img src="{{ $blog->imageblog }}" style="width:100%">
                 <p class="txtcontent">{{ $blog->title }}</p>
-                <!-- <p class="txtcontent">Digital Marketing là làm gì? Top những kỹ năng Digital Marketer phải biết Digital
-                    Marketing là một mảng nhỏ hơn của Marketing, yêu cầu những kiến </p> -->
+                <img src="{{ $blog->imageblog }}" style="width:100%">
                 <p class="txtcontent2">{!!  $blog->content !!}</p>
-                <!-- <p class="txtcontent2">Digital Marketing là một mảng nhỏ hơn của Marketing, yêu cầu những kiến thức
-                    chuyên sâu hơn các chiến lược marketing căn bản và khả năng sáng tạo.</p>
-                <p class="txtcontent3">Nếu bạn đang cân nhắc thử sức với ngành Digital Marketing thì hãy cùng tìm hiểu
-                    những kỹ năng cần có và công việc của một digital marketer là gì nhé.</p>
-                <p class="txtcontent3">Digital Marketer là ai?</p>
-                <p class="txtcontent3">Thông thường một digital marketer sẽ liên quan đến việc sử dụng các kênh digital
-                    để tạo ra leads và xây dựng brand awareness (nhận diện thương hiệu).</p>
-                <p class="txtcontent3">Các kênh digital thường gặp gồm có:</p>
-                <p class="txtcontent3">Website của công ty</p>
-                <p class="txtcontent3">Social media</p>
-                <p class="txtcontent3">Bảng xếp hạng công cụ tìm kiếm (SEM)</p>
-                <p class="txtcontent3">Email marketing</p>
-                <p class="txtcontent3">Quảng cáo online</p>
-                <p class="txtcontent3">Blog của công ty</p>
-                <p class="txtcontent3">Dựa vào đây, digital marketer phải sử dụng các công cụ đo lường để tìm ra điểm
-                    yếu và phương án cải thiện hiệu suất trên các kênh này. Tùy công ty, digital marketer có thể chịu
-                    trách nhiệm cho toàn bộ chiến lược digital của công ty hoặc chỉ tập trung vào một thứ.</p>
-                <p class="txtcontent3">Tại các công ty SMEs hoặc start-up thường có một chuyên gia hoặc một quản lý
-                    chung; trong khi đó ở các tập đoàn, trách nhiệm này có thể được phân bổ cho một team hoặc thậm chí
-                    cho nhiều bộ phận khác nhau có liên quan.</p>
-                <p class="txtcontent3">Đó là một lĩnh vực nơi kinh nghiệm hoặc thực tập tốt (internship) nói lên nhiều
-                    điều. “Certifications are the new degree,” một khẳng định nữa từ Justin Emig. Có rất nhiều chứng chỉ
-                    online bạn có thể thêm vào CV, tăng độ tin cậy với nhà tuyển dụng. Đây thường là cách tốt nhất để
-                    thu hút sự chú ý của nhà tuyển dụng nếu bạn muốn một công việc trong ngành digital marketing. Một
-                    điểm lưu ý nhỏ: Đừng cố gắng đề cập tất cả “thành tích” của bạn trên nền tảng online như là các
-                    trang mạng xã hội nghìn followers.</p>
-                <p class="txtcontent3">Top kỹ năng và hiểu biết cần có của Digital Marketer</p>
-                <p class="txtcontent3" style="font-weight:bold">1. Video</p>
-                <p class="txtcontent3">Có một thống kê chỉ ra rằng “Trong thời đại mà người dùng bị bủa vây bởi quá
-                    nhiều thông tin, mức độ tập trung của chúng ta giảm xuống khoảng 8,25 giây”. Vì vậy để thu hút được
-                    sự chú ý của người dùng online chưa bao giờ là chuyện dễ dàng. Các nghiên cứu đã cho thấy video giúp
-                    tăng lượng tương tác và tạo xếp hạng cao hơn trên Google. Bạn không cần trở thành video producer
-                    nhưng bạn có thể học cách tạo ra một video cơ bản. Hiểu cách viết kịch bản, sử dụng các nền tảng và
-                    apps để tạo ra video và các yếu tố ảnh hưởng của video sẽ là điểm cộng lớn cho bạn khi ứng tuyển cho
-                    công việc digital marketing.</p>
-                <p class="txtcontent3" style="font-weight:bold">2. SEO & SEM</p>
-                <p class="txtcontent3">Tìm kiếm online điều hướng quảng cáo digital. Bạn phải hiểu về tối ưu hóa công cụ
-                    tìm kiếm (SEO) và SEM (search engine marketing) nếu muốn làm việc trong ngành này. Bạn đừng cảm thấy
-                    quá lo lắng về sử dụng back-end. Hiểu về tầm quan trọng của SEO và cách ứng dụng của nó trong ngành
-                    còn quan trọng hơn rất nhiều. Đây là bước đầu tiên cảu bất cứ chiến dịch digital marketing hay quản
-                    trị nội dung nào. Hiểu cách SEO và SEM hoạt động và ảnh hưởng đến mục tiêu chung sẽ giúp bạn phối
-                    hợp tốt hơn với những mảng còn lại của team digital mà không cảm thấy lạc lõng hay ở nhầm chỗ.</p> -->
             </div>
         </div>
         <div class="d-flex flex-column" style="width: 32%;">
@@ -119,38 +75,6 @@
                     </div>
                 </a>
                 @endforeach
-                <!-- <div class="phanturightbox2 d-flex">
-                    <p class="txt6box2">02</p>
-                    <div style="margin-left:1em" class="d-flex flex-column">
-                        <span class="txt7box2">Digital Marketing là làm gì? Top những kỹ năng Digital Marketer phải biết
-                        </span>
-                        <span class="txt4box2">5 phút để đọc</span>
-                    </div>
-                </div>
-                <div class="phanturightbox2 d-flex">
-                    <p class="txt6box2">03</p>
-                    <div style="margin-left:1em" class="d-flex flex-column">
-                        <span class="txt7box2">Digital Marketing là làm gì? Top những kỹ năng Digital Marketer phải biết
-                        </span>
-                        <span class="txt4box2">5 phút để đọc</span>
-                    </div>
-                </div>
-                <div class="phanturightbox2 d-flex">
-                    <p class="txt6box2">04</p>
-                    <div style="margin-left:1em" class="d-flex flex-column">
-                        <span class="txt7box2">Digital Marketing là làm gì? Top những kỹ năng Digital Marketer phải biết
-                        </span>
-                        <span class="txt4box2">5 phút để đọc</span>
-                    </div>
-                </div>
-                <div class="phanturightbox2 d-flex">
-                    <p class="txt6box2">05</p>
-                    <div style="margin-left:1em" class="d-flex flex-column">
-                        <span class="txt7box2">Digital Marketing là làm gì? Top những kỹ năng Digital Marketer phải biết
-                        </span>
-                        <span class="txt4box2">5 phút để đọc</span>
-                    </div>
-                </div> -->
             </div>
         </div>
 
